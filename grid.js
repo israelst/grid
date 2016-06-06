d2d6630612e17f23fc5dfcf57b17799ce1921a56
Israel Teixeira <israelst@gmail.com>
2016-06-06 14:37:39 -0300

    Add horizontal grid to the current tab.

--- grid.js
+++ grid.js
@@ -1,2 +1,40 @@
-document.body.style.border = "5px solid red";
-console.log('upa');
+function addLines(){
+    var body = document.body,
+        root = body.parentElement,
+        style = getComputedStyle(root),
+        row = parseFloat(style.fontSize),
+        height = parseFloat(style.height),
+        qtyOfRows = Math.floor(height / row);
+        console.log(height, row)
+
+    for(var i = 0; i <= qtyOfRows; i++){
+        addLine(root, row, i);
+    }
+}
+
+function createLine(top){
+    var div = document.createElement('div');
+    div.style.width = '100%';
+    div.style.borderBottom = 'solid 1px';
+    div.style.zIndex= '999';
+
+    div.style.position = 'absolute';
+    div.style.top = top + 'px';
+
+    return div;
+}
+
+
+function addLine(parent, rowHeight, i){
+    var lineEl = createLine(i * rowHeight);
+    parent.appendChild(lineEl);
+}
+
+
+function grid(request, sender, sendResponse) {
+    console.log(request.qtyCols);
+    addLines();
+    chrome.runtime.onMessage.removeListener(grid);
+}
+
+chrome.runtime.onMessage.addListener(grid);
